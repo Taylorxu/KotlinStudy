eae0440f76a4a3624a1c50314a4afc822ea54943
Taylorxu <zhiguang_xu@live.cn>
2018-04-26 11:27:56 +0800

    搜索效果

--- app/src/main/java/com/xuzhiguang/kotlinstudybyself/forecast/navigationPages/FirstFragment.kt
+++ app/src/main/java/com/xuzhiguang/kotlinstudybyself/forecast/navigationPages/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.xuzhiguang.kotlinstudybyself.forecast.navigationPages
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.xuzhiguang.kotlinstudybyself.R
+import com.xuzhiguang.kotlinstudybyself.R.anim.search_in
 import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
@@ -14,7 +17,38 @@ import kotlinx.android.synthetic.main.fragment_first.*
 class FirstFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-       val view: View? = inflater?.inflate(R.layout.fragment_first,container,false)
+        var view: View? = inflater?.inflate(R.layout.fragment_first, container, false)
         return view?.rootView!!
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        bt_search.setOnClickListener { showSearchEdit() }
+        bt_search_cancel.setOnClickListener { hideSearchEdit() }
+    }
+
+    fun showSearchEdit() {
+        val searchPanelOut = AnimationUtils.loadAnimation(this.activity, R.anim.search_panel_out)
+        panel_frame.startAnimation(searchPanelOut)
+        panel_frame.visibility = View.GONE
+        search_layout.visibility = View.VISIBLE
+        val searchIn = AnimationUtils.loadAnimation(this.activity, R.anim.search_in)
+        search_layout.startAnimation(searchIn)
+
+    }
+
+    fun hideSearchEdit() {
+        panel_frame.visibility = View.VISIBLE
+        val searchPanelIn = AnimationUtils.loadAnimation(this.activity, R.anim.search_panel_in)
+        panel_frame.startAnimation(searchPanelIn)
+        search_layout.visibility = View.GONE
+        val searchOut = AnimationUtils.loadAnimation(this.activity, R.anim.search_out)
+        search_layout.startAnimation(searchOut)
+
     }
 }
